da07ec86ae362c021d03044357d3bebf07739871
xtophe38 <christophe.naud@gmail.com>
2023-09-25 15:56:01 +0200

    Set first release revision

--- module.combodo-ansible-playbooks.php
+++ module.combodo-ansible-playbooks.php
@@ -6,7 +6,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'combodo-ansible-playbooks/1.0.0-dev',
+	'combodo-ansible-playbooks/1.0.0',
 	array(
 		// Identification
 		//
